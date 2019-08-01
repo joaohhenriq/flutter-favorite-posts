79bc7b4f40947934c929c7ad17a4f3a46fe72874
joaoh <joao_camargo10@hotmail.com>
2019-07-31 23:31:58 -0300

    Finishing favorite bloc logic

--- lib/blocs/favorite_bloc.dart
+++ lib/blocs/favorite_bloc.dart
@@ -7,8 +7,6 @@ class FavoriteBloc extends BlocBase {
 
   final _favoritesModel = FavoritesModel();
 
-
-
   final _favoritesListController = BehaviorSubject<List<PostModel>>();
   Observable<List<PostModel>> get favoritesListStream => _favoritesListController.stream;
   Sink<List<PostModel>> get favoritesListSink => _favoritesListController.sink;
@@ -20,6 +18,25 @@ class FavoriteBloc extends BlocBase {
   final _favoritesListDeleteController = BehaviorSubject<PostModel>();
   Observable<PostModel> get favoritesDeleteStream => _favoritesListDeleteController.stream;
   Sink<PostModel> get favoritesDeleteSink => _favoritesListDeleteController.sink;
+
+  FavoriteBloc(){
+    favoritesAddStream.listen(_handleAdd);
+    favoritesDeleteStream.listen(_handleDelete);
+  }
+
+  //método para tratar quando usuário quiser adicionar post na lista de favoritos
+  void _handleAdd(PostModel postModel){
+    _favoritesModel.posts.add(postModel);
+  }
+
+  //método para atualizar a lista de favoritos com a lista mais atual de favoritos
+  void updateList(){
+    favoritesListSink.add(_favoritesModel.posts.toList());
+  }
+
+  void _handleDelete(PostModel postModel){
+    _favoritesModel.posts.remove(postModel);
+  }
 
   @override
   void dispose() {
